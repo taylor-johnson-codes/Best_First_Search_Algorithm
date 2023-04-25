a735d44e0df412a0227d48aa59bc992cf1cfc37e
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2023-04-24 22:21:00 -0700

    Add project files.

---
+++ Best First Search/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Best_First_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
